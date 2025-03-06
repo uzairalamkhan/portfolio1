ec820b12cd19cb41dd6c05a5ae71b39374548aea
Uzair Alam Khan <65907736+uzairalamkhan@users.noreply.github.com>
2025-03-06 17:08:53 +0530

    Update educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -2,19 +2,19 @@ export const educations = [
   {
     id: 1,
     title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    duration: "2019 - 2023",
+    institution: "Global Institute of Engineering and Technology",
   },
   {
     id: 2,
     title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    duration: "2016 - 2018",
+    institution: "Sri Chaitanya Jr College",
   },
   {
     id: 3,
     title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
-  }
+    duration: "",
+    institution: "Vikeananda High School",
+  },
 ]
