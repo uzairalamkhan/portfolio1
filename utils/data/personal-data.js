af523eaef212522213b2d84ce17530ef43cc333d
Uzair Alam Khan <65907736+uzairalamkhan@users.noreply.github.com>
2025-03-06 17:10:21 +0530

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,17 +1,15 @@
 export const personalData = {
-  name: "ABU SAID",
+  name: "Uzair Alam Khan",
   profile: '/profile.png',
   designation: "Software Developer",
-  description: "My name is ABU SAID. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
-  email: 'abusaid7388@gmail.com',
-  phone: '+8801608797655',
-  address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-  github: 'https://github.com/said7388',
-  facebook: 'https://www.facebook.com/abusaid.riyaz/',
-  linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-  twitter: 'https://twitter.com/said7388',
-  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-  leetcode: "https://leetcode.com/said3812/",
-  devUsername: "said7388",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+  description: "My name is Uzair Alam Khan. I am a professional Software Engineer with expertise in automation, low-code development, AI-driven SaaS, and cloud & DevOps (AWS & GCP).",
+  email: 'kuzair618@gmail.com',
+  phone: '+918522816768',
+  address: 'Hyderabad , India , 500008',
+  github: 'https://github.com/uzairalamkhan/',
+  facebook: '',
+  linkedIn: 'https://www.linkedin.com/in/uzair-alam-khan-70a272187/',
+  twitter: 'https://x.com/uzair_alam_khan?t=CTlhy1HjoW_3Iy3CxqhF5g&s=03',
+  devUsername: "Uzair-Ak",
+  resume: "https://drive.google.com/file/d/1ffJjrUab907Z7_E9oXVQTcmIIqv9JxUc/view?usp=sharing"
 }
