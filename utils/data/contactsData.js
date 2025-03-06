cdce2a5acbdf2ec9bb311c2de94b655e732c59dc
Uzair Alam Khan <65907736+uzairalamkhan@users.noreply.github.com>
2025-03-06 17:08:20 +0530

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+    email: 'kuzair618@gmail.com',
+    phone: '+918522816768',
+    address: 'Hyderabad , India , 500008',
+    github: 'https://github.com/uzairalamkhan/',
+    facebook: '',
+    linkedIn: 'https://www.linkedin.com/in/uzair-alam-khan-70a272187/',
+    twitter: 'https://x.com/uzair_alam_khan?t=CTlhy1HjoW_3Iy3CxqhF5g&s=03',
+    stackOverflow: '',
+    devUsername: "Uzair-Ak"
 }
